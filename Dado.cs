e221c8cf860096a47ae595891256c94f7bf24c6c
Carlosdz4 <Carlosdz4@DESKTOP-TSGDJTS>
2024-01-30 15:13:06 -0400

    Agregar archivos de proyecto.

---
+++ Dado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_Dados
+{
+    public class Dado
+    {
+
+        public int NumeroDado { get; set; }
+
+    }
+}
